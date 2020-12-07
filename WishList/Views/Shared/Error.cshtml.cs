75a72abe6b13ec42537b18b5b443d291efdb68b7
hacker1db <hacker1db@outlook.com>
2020-12-07 15:20:33 -0800

    Upated HTML on both home and Error pages.

---
+++ WishList/Views/Shared/Error.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WishList.Views.Shared
+{
+    public class Error : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
