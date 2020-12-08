1076c3c9b8d9b61e606afe67aff4809f914559a4
hacker1db <hacker1db@outlook.com>
2020-12-07 16:29:20 -0800

    Updates to pass local test.

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ WishList/Views/_ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WishList.Views
+{
+    public class _ViewStart : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
