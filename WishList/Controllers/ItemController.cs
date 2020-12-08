e82be99ea01f123c32da8873465786ad74bcd1c0
hacker1db <hacker1db@outlook.com>
2020-12-07 17:34:50 -0800

    added del and create and redirect actions.

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using WishList.Data;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
     public class ItemController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            throw new System.NotImplementedException();
+            var model = _context.Items.ToList()
+            return View("Index", model);
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        public IActionResult Create(Models.Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            _context.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    
     }
 }
